c85efd410a902db5b37567547959ae09d713119e
pkndiritu <nkirira@gmail.com>
2019-09-11 17:34:40 +0300

    data types

---
+++ variables.py
@@ -0,0 +1,10 @@
+# howards_marks = 45+67+78+45+56+67+78+67
+# print(howards_marks)
+# print((howards_marks)/8)
+
+# data types
+# Numbers - integers
+# #         - float
+# #         - double
+# # Boolean - True or False
+# # Strings
